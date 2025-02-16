d2c46b4bfe378bd307720278ccf387348c21c47f
shiroinekotfs <shiroineko.tfs@gmail.com>
2025-02-16 16:05:42 +0700

    update source

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hyper-V DDA GUI")]
+[assembly: AssemblyDescription("Hyper-V DDA, allowing hardware device being used natively by Virtual Machine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TheFlightSims")]
+[assembly: AssemblyProduct("Hyper-V DDA GUI")]
+[assembly: AssemblyCopyright("Copyright © TheFlightSims 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bda56c7f-5255-4e13-9e6d-1eff8792d948")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.2502")]
+[assembly: AssemblyFileVersion("1.0.0.2502")]
+[assembly: NeutralResourcesLanguage("en-US")]
