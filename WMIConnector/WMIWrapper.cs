78ed58747476503c7d2356e999300f6c5fddd35b
白いねこ <shiroineko.tfs@theflightsims.com>
2025-08-02 23:06:32 +0700

    .
    Co-Authored-By: Vo Luu Tuong Anh <anhvlt.2006@theflightsims.com>

--- WMIConnector/WMIWrapper.cs
+++ WMIConnector/WMIWrapper.cs
@@ -1,24 +1,7 @@
-﻿using System;
-using System.Management;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DDAGUI
+﻿namespace DDAGUI
 {
     class WMIWrapper
     {
-        public string CIMConnector = "";
-        public string WMIQuery = "";
-
-
-
-        public void AddDevice()
-        {
-
-        }
-
-
+    
     }
 }
