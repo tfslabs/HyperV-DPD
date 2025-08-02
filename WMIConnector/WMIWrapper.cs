79252c96701803e04c76cdc08e3812aae8a9663f
白いねこ <shiroineko.tfs@theflightsims.com>
2025-08-02 23:03:20 +0700

    Revert "."
    This reverts commit 00877207eb10a98f69b677d466b5262b05601000.

--- WMIConnector/WMIWrapper.cs
+++ WMIConnector/WMIWrapper.cs
@@ -1,7 +1,24 @@
-﻿namespace DDAGUI
+﻿using System;
+using System.Management;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDAGUI
 {
     class WMIWrapper
     {
+        public string CIMConnector = "";
+        public string WMIQuery = "";
+
+
+
+        public void AddDevice()
+        {
+
+        }
+
 
     }
 }
