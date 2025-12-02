61ad0a57dd09d0c5041307e5553d35c35c62045f
shiroinekotfs <shiroineko.tfs@theflightsims.com>
2025-12-02 16:24:42 +0700

    Update AddDevice.xaml.cs
    Co-Authored-By: VLTA of @TheFlightSims <176574466+anhvlttfs@users.noreply.github.com>

--- AddDevice.xaml.cs
+++ AddDevice.xaml.cs
@@ -2,16 +2,41 @@
 using System.Linq;
 using System.Management;
 using System.Windows;
+
 using TheFlightSims.HyperVDPD.DefaultUI;
 using TheFlightSims.HyperVDPD.WMIProperties;
 
+/*
+ * Primary namespace for HyperV-DPD application
+ *  It contains the main window and all related methods for the core application
+ */
 namespace TheFlightSims.HyperVDPD
 {
+    /*
+     * Add Device Window class
+     * It is used to add device into the selected VM
+     */
     public partial class AddDevice : Window
     {
+        ////////////////////////////////////////////////////////////////
+        /// Global Properties and Constructors Region
+        ///     This region contains global properties and constructors 
+        ///     for the MachineMethods class.
+        ////////////////////////////////////////////////////////////////
+
+        /*
+         *  Global properties
+         *      deviceId: Device ID of the selected device
+         *      machine: WMI namespace of the machine
+         */
+
         protected string deviceId;
         protected MachineMethods machine;
 
+        /*
+         * Constructor of the MainWindow class
+         * Initializes the components and sets up event handlers
+         */
         public AddDevice(MachineMethods machine)
         {
             this.machine = machine;
@@ -19,12 +44,19 @@ namespace TheFlightSims.HyperVDPD
             InitializeComponent();
         }
 
-        /*
-         * Button and UI methods behaviour....
-         */
 
+        ////////////////////////////////////////////////////////////////
+        /// User Action Methods Region
+        ///     This region contains methods that handle user actions.
+        ///     For example, button clicks, changes in order.
+        ////////////////////////////////////////////////////////////////
+
+        /*
+         * Actions for button "Add Device"
+         */
         private void AddDeviceButton_Click(object sender, RoutedEventArgs e)
         {
+            // If the selected item is not null (nothing is selected), proceed
             if (DeviceList.SelectedItem != null)
             {
                 deviceId = DeviceList.SelectedItem.GetType().GetProperty("DeviceId").GetValue(DeviceList.SelectedItem, null).ToString();
@@ -32,6 +64,7 @@ namespace TheFlightSims.HyperVDPD
             }
             else
             {
+                // Notify user if nothing is selected
                 _ = MessageBox.Show(
                     "Please select a device to add.",
                     "Warning",
@@ -41,29 +74,47 @@ namespace TheFlightSims.HyperVDPD
             }
         }
 
+        /*
+         * Actions for button "Close"
+         */
         private void AddDeviceCloseButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
 
-        /*
-         * Non-button methods
-         */
+        ////////////////////////////////////////////////////////////////
+        /// Non-User Action Methods Region
+        /// 
+        /// This region contains methods that do not handle user actions.
+        /// 
+        /// Think about this is the back-end section.
+        /// It should not be in a seperated class, because it directly interacts with the UI elements.
+        ////////////////////////////////////////////////////////////////
 
+        /*
+         * Start getting the device ID
+         */
         public string GetDeviceId()
         {
+
             UpdateDevices();
             _ = ShowDialog();
 
+            // Return the device ID back to the main windows
             return deviceId;
         }
 
-        private void UpdateDevices()
+        /*
+         * Update device list
+         */
+        private async void UpdateDevices()
         {
+            // Clear the device list
             DeviceList.Items.Clear();
 
             try
             {
+                // Loop for devices in Win32_PnPEntity
                 foreach (ManagementObject device in machine.GetObjects("Win32_PnPEntity", "Status, PNPClass, Name, DeviceID").Cast<ManagementObject>())
                 {
                     string deviceStatus = device["Status"]?.ToString() ?? "Unknown";
@@ -71,8 +122,10 @@ namespace TheFlightSims.HyperVDPD
                     string deviceName = device["Name"]?.ToString() ?? "Unknown";
                     string deviceId = device["DeviceID"]?.ToString() ?? "Unknown";
 
+                    // If the device is not PCI, skip to the next device
                     if (!deviceId.StartsWith("PCI"))
                     {
+                        device.Dispose();
                         continue;
                     }
 
@@ -83,6 +136,8 @@ namespace TheFlightSims.HyperVDPD
                         DeviceName = deviceName,
                         DeviceId = deviceId
                     });
+
+                    device.Dispose();
                 }
             }
             catch (Exception ex)
