f10a585321c2eb54d1c13898294d6324dda36d47
shiroinekotfs <shiroineko.tfs@theflightsims.com>
2025-12-02 11:46:15 +0700

    update for about window
    Co-Authored-By: VLTA of @TheFlightSims <176574466+anhvlttfs@users.noreply.github.com>

---
+++ DefaultUI/About.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+/* The Default UI includes
+ * 1. ConnectForm.xaml - A form to connect to a local or remote computer
+ * 2. ExceptionView.xaml - A window to handle exception messages
+ * 3. About.xaml - A window to display information about the application
+ */
+namespace TheFlightSims.HyperVDPD.DefaultUI
+{
+    /*
+     * About Form class
+     * It is used to display the help info & Legal notice
+     */
+    public partial class About : Window
+    {
+        // Constructor of the menu
+        public About()
+        {
+            InitializeComponent();
+        }
+    }
+}
