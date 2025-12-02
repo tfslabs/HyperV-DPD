345aa089ee216c30f287eecce50294273b32b7a9
shiroinekotfs <shiroineko.tfs@theflightsims.com>
2025-12-03 01:05:27 +0700

    Update CheckForAssignableDevice.xaml.cs
    Co-Authored-By: VLTA of @TheFlightSims <176574466+anhvlttfs@users.noreply.github.com>

--- CheckForAssignableDevice.xaml.cs
+++ CheckForAssignableDevice.xaml.cs
@@ -7,34 +7,72 @@ using System.Windows;
 using TheFlightSims.HyperVDPD.DefaultUI;
 using TheFlightSims.HyperVDPD.WMIProperties;
 
+/*
+ * Primary namespace for HyperV-DPD application
+ *  It contains the main window and all related methods for the core application
+ */
 namespace TheFlightSims.HyperVDPD
 {
+    /*
+     * Check for assignable device window
+     * It is used to check for devices that are assignable for PCIP
+     */
     public partial class CheckForAssignableDevice : Window
     {
+        ////////////////////////////////////////////////////////////////
+        /// Global Properties and Constructors Region
+        ///     This region contains global properties and constructors 
+        ///     for the MachineMethods class.
+        ////////////////////////////////////////////////////////////////
+
+        /*
+         * Global properties
+         *  machine: Instance of MachineMethods class to interact with WMI
+         */
         protected MachineMethods machine;
 
+        /*
+         * Constructor of the class
+         */
         public CheckForAssignableDevice(MachineMethods machine)
         {
             this.machine = machine;
-
             InitializeComponent();
         }
 
+        ////////////////////////////////////////////////////////////////
+        /// User Action Methods Region
+        ///     This region contains methods that handle user actions.
+        ///     For example, button clicks, changes in order.
+        ////////////////////////////////////////////////////////////////
+        
         /*
-         * Button and UI methods behaviour....
+         * Actions for "Check" button
          */
         private async void Check_Button(object sender, RoutedEventArgs e)
         {
             await CheckDevices();
         }
 
+        /*
+         * Actions for "Cancel" button
+         */
         private void Cancel_Button(object sender, RoutedEventArgs e)
         {
             Close();
         }
 
+        ////////////////////////////////////////////////////////////////
+        /// Non-User Action Methods Region
+        /// 
+        /// This region contains methods that do not handle user actions.
+        /// 
+        /// Think about this is the back-end section.
+        /// It should not be in a seperated class, because it directly interacts with the UI elements.
+        ////////////////////////////////////////////////////////////////
+        
         /*
-         * Non-button methods
+         * Task for Check devices
          */
         private async Task CheckDevices()
         {
@@ -49,105 +87,119 @@ namespace TheFlightSims.HyperVDPD
 
                     // API calls to get all PnP entities
                     ManagementObjectCollection pnpDeviceEntity = machine.GetObjects("Win32_PnPEntity", "DeviceID, Caption, Status");
-                    ManagementObjectCollection pnpDevice = machine.GetObjects("Win32_PnPDevice", "SameElement, SystemElement");
+                    ManagementObjectCollection pnpDevice = machine.GetObjects("Win32_PnPDevice", "SystemElement");
                     ManagementObjectCollection pnpDeviceAllocatedResource = machine.GetObjects("Win32_PNPAllocatedResource", "Antecedent, Dependent");
+                    ManagementObjectCollection pnpDeviceMemorySpace = machine.GetObjects("Win32_DeviceMemoryAddress", "StartingAddress, EndingAddress");
 
-
-
-                    foreach (ManagementObject device in machine.GetObjects("Win32_PnPEntity", "DeviceID, Caption, Status").Cast<ManagementObject>())
+                    Dictionary<string, (string caption, string status)> pnpDeviceEntityMap = new Dictionary<string, (string, string)>();
+                    foreach (ManagementObject device in pnpDeviceEntity.Cast<ManagementObject>())
                     {
                         string deviceId = device["DeviceID"]?.ToString();
                         string devCaption = device["Caption"]?.ToString();
                         string devStatus = device["Status"]?.ToString();
 
-                        if (deviceId == null || devCaption == null || devStatus == null)
+                        if (!string.IsNullOrEmpty(deviceId) && !string.IsNullOrEmpty(devCaption) && !string.IsNullOrEmpty(devStatus))
                         {
-                            device.Dispose();
-                            continue;
+                            pnpDeviceEntityMap.Add(deviceId.Replace("\\\\", "\\").Trim(), (devCaption, devStatus));
                         }
+
+                        device.Dispose();
+                    }
+
+                    HashSet<string> pnpDeviceSet = new HashSet<string>();
+                    foreach (ManagementObject device in pnpDevice.Cast<ManagementObject>())
+                    {
+                        string PnpDevString = device["SystemElement"]?.ToString();
+
+                        if (!string.IsNullOrEmpty(PnpDevString))
+                        {
+                            string actualPnpDevString = PnpDevString.Split('=')[1].Replace("\"", "").Replace("\\\\", "\\").Trim();
+                            pnpDeviceSet.Add(actualPnpDevString);
+                        }
+
+                        device.Dispose();
+                    }
+
+                    List<(UInt64 startingMemory, string deviceID)> pnpDeviceAllocatedResourceMap = new List<(UInt64 startingMemory, string deviceID)>();
+                    foreach (ManagementObject resource in pnpDeviceAllocatedResource.Cast<ManagementObject>())
+                    {
+                        string antecedentRes = resource["Antecedent"]?.ToString();
+                        string dependentRes = resource["Dependent"]?.ToString();
+
+                        if (!string.IsNullOrEmpty(antecedentRes) && !string.IsNullOrEmpty(dependentRes))
+                        {
+                            UInt64 startingMemory = UInt64.Parse(antecedentRes.Split('=')[1].Replace("\"", ""));
+                            string deviceID = dependentRes.Split('=')[1].Replace("\"", "").Replace("\\\\", "\\").Trim();
+
+                            pnpDeviceAllocatedResourceMap.Add((
+                                    startingMemory,
+                                    deviceID
+                            ));
+                        }
+
+                        resource.Dispose();
+                    }
+
+                    Dictionary<UInt64, UInt64> pnpDeviceMemorySpaceMap = new Dictionary<UInt64, UInt64>();
+                    foreach (ManagementObject memorySpaces in pnpDeviceMemorySpace.Cast<ManagementObject>())
+                    {
+                        string startingAddress = memorySpaces["StartingAddress"]?.ToString();
+                        string endingAddress = memorySpaces["EndingAddress"]?.ToString();
+
+                        if (!string.IsNullOrEmpty(startingAddress) && !string.IsNullOrEmpty(endingAddress))
+                        {
+                            pnpDeviceMemorySpaceMap.Add(
+                                UInt64.Parse(startingAddress),
+                                UInt64.Parse(endingAddress)
+                            );
+                        }
+
+                        memorySpaces.Dispose();
+                    }
+
+                    
+                    /*
+                     * 
+                     */
+                    foreach (KeyValuePair<string, (string devCaption, string devStatus)> deviceInstance in pnpDeviceEntityMap)
+                    {
+                        string deviceId = deviceInstance.Key;
+                        string caption = deviceInstance.Value.devCaption;
+                        string status = deviceInstance.Value.devStatus;
 
                         if (deviceId.StartsWith("PCI\\"))
                         {
                             bool isAssignable = true;
-                            HashSet<string> startingAddresses = new HashSet<string>();
+                            UInt64 memoryGap = 0;
                             string deviceNote = string.Empty;
-                            double memoryGap = 0;
 
-                            foreach (ManagementObject actualPnPDevice in machine.GetObjects("Win32_PnPDevice", "SameElement, SystemElement").Cast<ManagementObject>())
+                            if (!status.ToLower().Equals("ok"))
                             {
-                                string actualPnpDevString = actualPnPDevice["SystemElement"]?.ToString();
-
-                                if (actualPnpDevString == null)
-                                {
-                                    isAssignable = false;
-                                    actualPnPDevice.Dispose();
-                                    continue;
-                                }
-
-                                if (actualPnpDevString.Contains(deviceId.Replace("\\", "\\\\")))
-                                {
-                                    isAssignable = true;
-                                    break;
-                                }
-                                else
-                                {
-                                    isAssignable = false;
-                                }
-
-                                actualPnPDevice.Dispose();
-                            }
-
-                            if (!isAssignable)
-                            {
-                                deviceNote += ((deviceNote.Length == 0) ? "" : "\n") + "The PCI device is not support either Express Endpoint, Embedded Endpoint, or Legacy Express Endpoint.";
-                            }
-
-                            foreach (ManagementObject deviceResource in machine.GetObjects("Win32_PNPAllocatedResource", "Antecedent, Dependent").Cast<ManagementObject>())
-                            {
-                                string devResDependent = deviceResource["Dependent"]?.ToString();
-
-                                if (devResDependent == null)
-                                {
-                                    deviceResource.Dispose();
-                                    continue;
-                                }
-
-                                if (devResDependent.Contains(deviceId.Replace("\\", "\\\\")))
-                                {
-                                    _ = startingAddresses.Add((deviceResource["Antecedent"].ToString().Split('=')[1]).Replace("\"", ""));
-                                }
-
-                                deviceResource.Dispose();
-                            }
-
-                            if (!devStatus.ToLower().Equals("ok"))
-                            {
-                                deviceNote += ((deviceNote.Length == 0) ? "" : "\n") + "Device is disabled. Please re-enable the device to let the program check the memory gap.";
                                 isAssignable = false;
+                                deviceNote += ((deviceNote.Length == 0) ? "" : "\n") + "Device is disabled. Please re-enable the device to let the program check the memory gap.";
+                            }
+
+                            if (!pnpDeviceSet.Contains(deviceId))
+                            {
+                                isAssignable = false;
+                                deviceNote += ((deviceNote.Length == 0) ? "" : "\n") + "The PCI device is not support either Express Endpoint, Embedded Endpoint, or Legacy Express Endpoint.";
                             }
 
                             if (isAssignable)
                             {
-                                foreach (ManagementObject deviceMem in machine.GetObjects("Win32_DeviceMemoryAddress", "StartingAddress, EndingAddress").Cast<ManagementObject>())
+                                foreach ((UInt64 startMemory, string deviceId) deviceResource in pnpDeviceAllocatedResourceMap)
                                 {
-                                    string startAddrString = deviceMem["StartingAddress"]?.ToString();
-                                    string endAddrString = deviceMem["EndingAddress"]?.ToString();
-
-                                    if (startAddrString == null || endAddrString == null)
+                                    if (deviceId.Equals(deviceResource.deviceId))
                                     {
-                                        deviceMem.Dispose();
-                                        continue;
+                                        if (pnpDeviceMemorySpaceMap.ContainsKey(deviceResource.startMemory))
+                                        {
+                                            UInt64 endAddr = pnpDeviceMemorySpaceMap[deviceResource.startMemory];
+                                            memoryGap += (endAddr - deviceResource.startMemory + 1048575) / 1048576;
+                                        }
                                     }
-
-                                    if (startingAddresses.Contains(startAddrString))
-                                    {
-                                        memoryGap += Math.Ceiling((double.Parse(endAddrString) - double.Parse(startAddrString)) / 1048576.0);
-                                    }
-
-                                    deviceMem.Dispose();
                                 }
                             }
-
+                            
                             if (memoryGap == 0)
                             {
                                 deviceNote += ((deviceNote.Length == 0) ? "" : "\n") + "Unable to calculate gap memory.";
@@ -158,14 +210,12 @@ namespace TheFlightSims.HyperVDPD
                                 _ = AssignableDevice_ListView.Items.Add(new
                                 {
                                     DeviceId = deviceId,
-                                    DeviceName = devCaption,
+                                    DeviceName = caption,
                                     DeviceGap = (!isAssignable) ? "N/A" : $"{memoryGap} MB",
                                     DeviceNote = deviceNote
                                 });
                             });
                         }
-
-                        device.Dispose();
                     }
                 });
             }
