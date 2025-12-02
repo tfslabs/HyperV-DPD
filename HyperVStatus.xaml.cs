a160b112cf33e625c9cb6ede4a2165cde2a09a2b
shiroinekotfs <shiroineko.tfs@theflightsims.com>
2025-12-02 11:49:03 +0700

    Update HyperVStatus.xaml.cs
    Co-Authored-By: VLTA of @TheFlightSims <176574466+anhvlttfs@users.noreply.github.com>

--- HyperVStatus.xaml.cs
+++ HyperVStatus.xaml.cs
@@ -1,15 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Management;
 using System.Windows;
+using TheFlightSims.HyperVDPD.DefaultUI;
 using TheFlightSims.HyperVDPD.WMIProperties;
 
+/*
+ * Primary namespace for HyperV-DPD application
+ *  It contains the main window and all related methods for the core application
+ */
 namespace TheFlightSims.HyperVDPD
 {
+    /*
+     * Hyper-V Services Status Window
+     */
     public partial class HyperVStatus : Window
     {
+        ////////////////////////////////////////////////////////////////
+        /// Global Properties and Constructors Region
+        ///     This region contains global properties and constructors 
+        ///     for the MachineMethods class.
+        ////////////////////////////////////////////////////////////////
+        
+        
         protected MachineMethods machine;
-
+        public static readonly HashSet<string> serviceNames = new HashSet<string>()
+        {
+            "HvHost",
+            "vmickvpexchange",
+            "gcs",
+            "vmicguestinterface",
+            "vmicshutdown",
+            "vmicheartbeat",
+            "vmcompute",
+            "vmicvmsession",
+            "vmicrdv",
+            "vmictimesync",
+            "vmms",
+            "vmicvss"
+        };
 
         public HyperVStatus(MachineMethods machine)
         {
@@ -39,13 +69,13 @@ namespace TheFlightSims.HyperVDPD
                 machine.Connect("root\\cimv2");
                 foreach (ManagementObject srv in machine.GetObjects("Win32_Service", "Name, Caption, State").Cast<ManagementObject>())
                 {
-                    if (srv["Name"] == null || !WMIDefaultValues.serviceNames.Contains(srv["Name"].ToString()))
+                    if (srv["Name"] == null || !serviceNames.Contains(srv["Name"].ToString()))
                     {
                         srv.Dispose();
                         continue;
                     }
 
-                    ListHVSrv.Items.Add(new
+                    _ = ListHVSrv.Items.Add(new
                     {
                         ServiceName = srv["Caption"]?.ToString() ?? "Unknown",
                         ServiceStatus = srv["State"]?.ToString() ?? "Unknown"
@@ -56,7 +86,7 @@ namespace TheFlightSims.HyperVDPD
             }
             catch (Exception ex)
             {
-                WMIDefaultValues.HandleException(ex, machine.GetComputerName());
+                (new ExceptionView()).HandleException(ex);
             }
         }
     }
