1f4360222ca81763f7a8d4471ae0156ef385a038
shiroinekotfs <shiroineko.tfs@theflightsims.com>
2025-12-03 22:51:51 +0700

    update for non-pci device list
    Co-Authored-By: VLTA of @TheFlightSims <176574466+anhvlttfs@users.noreply.github.com>

--- CheckForAssignableDevice.xaml.cs
+++ CheckForAssignableDevice.xaml.cs
@@ -45,7 +45,7 @@ namespace TheFlightSims.HyperVDPD
         ///     This region contains methods that handle user actions.
         ///     For example, button clicks, changes in order.
         ////////////////////////////////////////////////////////////////
-        
+
         /*
          * Actions for "Check" button
          */
@@ -70,15 +70,59 @@ namespace TheFlightSims.HyperVDPD
         /// Think about this is the back-end section.
         /// It should not be in a seperated class, because it directly interacts with the UI elements.
         ////////////////////////////////////////////////////////////////
-        
+
         /*
          * Task for Check devices
          */
         private async Task CheckDevices()
         {
-
+            // Clear the device list
             AssignableDevice_ListView.Items.Clear();
 
+            /*
+             * Check for assignable devices
+             * 
+             * How does it work?
+             *    1. It looks up for device data in
+             *      - Win32_PnPEntity, to get its Hardware ID, Hardware Friendly Name (Caption), and current status. 
+             *          Construct all data into a list, follows this structure of a tuple:
+             *              (string deviceID, string devCaption, devStatus)
+             *      
+             *      - Win32_PnPDevice, to find if the device is actual device
+             *          Construct all data in a hash set, where the devices are Hardware ID unique.
+             *      
+             *      - Win32_PNPAllocatedResource, to find the starting address of the Hardware ID. 
+             *          Note: A device may have multiple starting memory
+             *          Construct into a list, follows this structure:
+             *              (UInt64 startingAddress, string deviceID)
+             *      
+             *      - Win32_DeviceMemoryAddress, to look up for possible address range, and calculate the required memory
+             *          Construct into a hash map, follows this structure:
+             *              <TKey: startingAddress, TValue: endingAddress>
+             *
+             *    2. For each device data, put into the boxes. Converting values from MOF structure into processable data can be seen below.
+             *    3. From the data structure, try to validate if a device from the Win32_PnPEntity matches the following condition
+             *          - The device is PCI device (required)
+             *          - The device exists in Win32_PnPDevice (required)
+             *          - If possible, check for the memory gap. Note that if the device is disabled, the gap cannot be calculated
+             *       If any the required condition does not meet, notify user for the "unable to assignment" and display for memory gap as "N/A"
+             *       If unable to calculate the gap, just display "N/A"
+             *  
+             * Note:
+             *      - All device ID must be normalized with a single "\" and not "\\"
+             *      - Normally, the WMI object's property is formed into multivalue, and value seperation with validation is required.
+             *      - Check for null, empty, or default (fallback) value in each case you get the value from the WMI.
+             *      - Disposing the resouce after using it
+             * 
+             * References:
+             *      - https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/win32-pnpentity
+             *      - https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/win32-pnpdevice
+             *      - https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/win32-pnpallocatedresource
+             *      - https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/win32-devicememoryaddress
+             * 
+             * Limitation:
+             *      Each resource has its own limitations. See below
+             */
             try
             {
                 await Task.Run(() =>
@@ -91,6 +135,16 @@ namespace TheFlightSims.HyperVDPD
                     ManagementObjectCollection pnpDeviceAllocatedResource = machine.GetObjects("Win32_PNPAllocatedResource", "Antecedent, Dependent");
                     ManagementObjectCollection pnpDeviceMemorySpace = machine.GetObjects("Win32_DeviceMemoryAddress", "StartingAddress, EndingAddress");
 
+                    /*
+                     * Device list on the computer
+                     * 
+                     * pnpDeviceEntityMap - List
+                     * 
+                     * Data structure
+                     *  deviceId (string): The ID of the device on the system. Example: PCI\VEN_10DE&DEV_2520&SUBSYS_12F21462&REV_A1
+                     *  devCaption (string): Actual device name. Example: NVIDIA GeForce RTX 3060 Laptop GPU
+                     *  devStatus (string): The status of the device. See the Win32_PnPEntity for detailed status info
+                     */
                     List<(string deviceId, string devCaption, string devStatus)> pnpDeviceEntityMap = new List<(string, string, string)>();
                     foreach (ManagementObject device in pnpDeviceEntity.Cast<ManagementObject>())
                     {
@@ -100,65 +154,112 @@ namespace TheFlightSims.HyperVDPD
 
                         if (!string.IsNullOrEmpty(deviceId) && !string.IsNullOrEmpty(devCaption) && !string.IsNullOrEmpty(devStatus))
                         {
+                            // Don't forget to normalize the device id
                             pnpDeviceEntityMap.Add(
                                 (deviceId.Replace("\\\\", "\\").Trim(), devCaption, devStatus)
                             );
                         }
 
+                        // Dispose after using it
                         device.Dispose();
                     }
 
+                    /*
+                     * PnP device on the computer
+                     * 
+                     * pnpDeviceSet - Hash Set
+                     * 
+                     * Data structure
+                     *  actualPnpDevString (string): the device id exist on the class
+                     *  
+                     * How does the WMI property contribute for data
+                     *  SystemElement: \\<Computer Name>\root\cimv2:Win32_PnPEntity.DeviceID="<Un-normalized device ID data>"
+                     * 
+                     */
                     HashSet<string> pnpDeviceSet = new HashSet<string>();
                     foreach (ManagementObject device in pnpDevice.Cast<ManagementObject>())
                     {
                         string PnpDevString = device["SystemElement"]?.ToString();
-
+                        
                         if (!string.IsNullOrEmpty(PnpDevString))
                         {
-                            string[] sysElementStrArr = PnpDevString.Split('=');
-
-                            if (sysElementStrArr.Length < 2)
+                            // Seperate between the path and pointer
+                            string[] SystemElementArr = PnpDevString.Split(':');
+                            
+                            // In case the pointer does not exist (Really?)
+                            if (SystemElementArr.Length < 2)
                             {
+                                // Dispose the device and go on
+                                device.Dispose();
                                 continue;
                             }
 
-                            string actualPnpDevString = sysElementStrArr[1].Replace("\"", "").Replace("\\\\", "\\").Trim('"');
-
-
-                            pnpDeviceSet.Add(actualPnpDevString);
+                            // The array[1] is the pointer.
+                            string actualPnpDevString = SystemElementArr[1].Replace("Win32_PnPEntity.DeviceID=", "").Trim('"');
+                            
+                            // Add into the hash set. Don't forget to normalize
+                            pnpDeviceSet.Add(actualPnpDevString.Replace("\\\\", "\\"));
                         }
 
+                        // Dispose the device instance
                         device.Dispose();
                     }
 
-                    List<(UInt64 startingMemory, string deviceID)> pnpDeviceAllocatedResourceMap = new List<(UInt64 startingMemory, string deviceID)>();
+                    /*
+                     * PnP Device Allocation Map
+                     * 
+                     * pnpDeviceAllocatedResourceMap - List
+                     * 
+                     * Data structure
+                     *  startingAddress (UInt64): starting address
+                     *  deviceID (string): device ID
+                     *  
+                     * How does the WMI property contribute for data
+                     *  Antecedent: \\<Computer Name>\root\cimv2:Win32_DeviceMemoryAddress.StartingAddress="<Starting Address in UInt64>"
+                     *  Dependent: \\<Computer Name>\root\cimv2:Win32_PnPEntity.DeviceID="<Un-normalized device ID data>"
+                     */
+                    List<(UInt64 startingAddress, string deviceID)> pnpDeviceAllocatedResourceMap = new List<(UInt64 startingAddress, string deviceID)>();
                     foreach (ManagementObject resource in pnpDeviceAllocatedResource.Cast<ManagementObject>())
                     {
                         string antecedentRes = resource["Antecedent"]?.ToString();
                         string dependentRes = resource["Dependent"]?.ToString();
-
+                        
                         if (!string.IsNullOrEmpty(antecedentRes) && !string.IsNullOrEmpty(dependentRes))
                         {
-                            string[] antecedentResStrArr = antecedentRes.Split('=');
-                            string[] dependentResStrArr = dependentRes.Split('=');
+                            // Seperate between the path and pointer
+                            string[] AntecedentArr = antecedentRes.Split(':');
+                            string[] DependentArr = dependentRes.Split(':');
 
-                            if (antecedentResStrArr.Length < 2 || dependentResStrArr.Length < 2)
+                            // In case the pointer does not exist (Really?)
+                            if (AntecedentArr.Length < 2 || DependentArr.Length < 2)
                             {
+                                // Dispose the device and go on
+                                resource.Dispose();
                                 continue;
                             }
 
-                            UInt64 startingMemory = UInt64.Parse(antecedentResStrArr[1].Replace("\"", ""));
-                            string deviceID = dependentResStrArr[1].Replace("\"", "").Replace("\\\\", "\\").Trim('"');
+                            // The array[1] is the pointer.
+                            UInt64 startingAddress = UInt64.Parse(AntecedentArr[1].Split('=')[1].Trim('"'));
+                            string deviceID = DependentArr[1].Replace("Win32_PnPEntity.DeviceID=", "").Trim('"');
 
-                            pnpDeviceAllocatedResourceMap.Add((
-                                    startingMemory,
-                                    deviceID
-                            ));
+                            // Add into the hash set. Don't forget to normalize
+                            pnpDeviceAllocatedResourceMap.Add(
+                                (startingAddress, deviceID.Replace("\\\\", "\\"))
+                            );
                         }
 
                         resource.Dispose();
                     }
 
+                    /*
+                     * PnP Device Memory Space Map
+                     * 
+                     * pnpDeviceMemorySpaceMap - Dictionary/Hash Map
+                     * 
+                     * Data structure
+                     *  startingAddress (UInt64): Starting address (unique)
+                     *  endingAddress (UInt64): Ending address
+                     */
                     Dictionary<UInt64, UInt64> pnpDeviceMemorySpaceMap = new Dictionary<UInt64, UInt64>();
                     foreach (ManagementObject memorySpaces in pnpDeviceMemorySpace.Cast<ManagementObject>())
                     {
@@ -176,65 +277,63 @@ namespace TheFlightSims.HyperVDPD
                         memorySpaces.Dispose();
                     }
 
+                    // Dispose all resources, since the data has been processed
                     pnpDeviceEntity.Dispose();
                     pnpDevice.Dispose();
                     pnpDeviceAllocatedResource.Dispose();
                     pnpDeviceMemorySpace.Dispose();
 
-                    /*
-                     * 
-                     */
                     foreach ((string id, string caption, string status) in pnpDeviceEntityMap)
                     {
-                        if (id.StartsWith("PCI\\"))
+                        bool isPciDevice = true, isActualDevice = true, isEnabled = true;
+                        string deviceNote = string.Empty;
+                        UInt64 memoryGap = 0;
+
+                        if (!id.StartsWith("PCI\\"))
                         {
-                            bool isAssignable = true;
-                            UInt64 memoryGap = 0;
-                            string deviceNote = string.Empty;
+                            isPciDevice = false;
+                            deviceNote += ((deviceNote.Length == 0) ? "" : "\n") + "The device is not a PCI.";
+                        }
 
-                            if (!string.Equals(status, "OK", StringComparison.OrdinalIgnoreCase))
-                            {
-                                isAssignable = false;
-                                deviceNote += ((deviceNote.Length == 0) ? "" : "\n") + "Device is disabled. Please re-enable the device to let the program check the memory gap.";
-                            }
+                        if (!pnpDeviceSet.Contains(id))
+                        {
+                            isActualDevice = false;
+                            deviceNote += ((deviceNote.Length == 0) ? "" : "\n") +
+                                "The PCI device is not support either Express Endpoint, Embedded Endpoint, or Legacy Express Endpoint.";
+                        }
 
-                            if (!pnpDeviceSet.Contains(id))
-                            {
-                                isAssignable = false;
-                                deviceNote += ((deviceNote.Length == 0) ? "" : "\n") + "The PCI device is not support either Express Endpoint, Embedded Endpoint, or Legacy Express Endpoint.";
-                            }
+                        if (!string.Equals(status, "OK", StringComparison.OrdinalIgnoreCase))
+                        {
+                            isEnabled = false;
+                            deviceNote += ((deviceNote.Length == 0) ? "" : "\n") +
+                                "Device is disabled. Please re-enable the device to let the program check the memory gap.";
+                        }
 
-                            if (isAssignable)
+                        if (isPciDevice && isActualDevice && isEnabled)
+                        {
+                            foreach ((UInt64 startMemory, string deviceId) in pnpDeviceAllocatedResourceMap)
                             {
-                                foreach ((UInt64 startMemory, string deviceId) in pnpDeviceAllocatedResourceMap)
+                                if (id.Equals(deviceId))
                                 {
-                                    if (id.Equals(deviceId))
+                                    if (pnpDeviceMemorySpaceMap.ContainsKey(startMemory))
                                     {
-                                        if (pnpDeviceMemorySpaceMap.ContainsKey(startMemory))
-                                        {
-                                            UInt64 endAddr = pnpDeviceMemorySpaceMap[startMemory];
-                                            memoryGap += (endAddr - startMemory + 1048575UL) / 1048576UL;
-                                        }
+                                        UInt64 endAddr = pnpDeviceMemorySpaceMap[startMemory];
+                                        memoryGap += (endAddr - startMemory + 1048575UL) / 1048576UL;
                                     }
                                 }
                             }
-                            
-                            if (memoryGap == 0)
-                            {
-                                deviceNote += ((deviceNote.Length == 0) ? "" : "\n") + "Unable to calculate gap memory.";
-                            }
-
-                            Dispatcher.Invoke(() =>
-                            {
-                                _ = AssignableDevice_ListView.Items.Add(new
-                                {
-                                    DeviceId = id,
-                                    DeviceName = caption,
-                                    DeviceGap = (!isAssignable) ? "N/A" : $"{memoryGap} MB",
-                                    DeviceNote = deviceNote
-                                });
-                            });
                         }
+
+                        Dispatcher.Invoke(() =>
+                        {
+                            _ = AssignableDevice_ListView.Items.Add(new
+                            {
+                                DeviceId = id,
+                                DeviceName = caption,
+                                DeviceGap = (isPciDevice && isActualDevice && isEnabled) ? $"{memoryGap} MB" : "N/A",
+                                DeviceNote = deviceNote
+                            });
+                        });
                     }
                 });
             }
